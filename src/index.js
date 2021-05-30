9a108c8d09f428e12247ab50d03d00b55ce22329
Alexandra Diem <alexandra.diem@gmail.com>
2021-05-30 21:01:56 +0200

    wip input form

--- src/index.js
+++ src/index.js
@@ -13,47 +13,73 @@ class ChallengeInputForm extends React.Component {
             challengeEndDate: new Date(),
             challengePassphrase: ''
         };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.saveChallenge = this.saveChallenge.bind(this);
     }
-  
+
     render() {
         return (
             <div>
                 <h3>Open a new challenge:</h3>
-                <form id="challengeInputForm">
-                <div class="form-group">
-                    <label for="challengeSegment">Link to Strava segment </label>
-                    <input type="text" class="form-control" id="challengeSegment" placeholder="Link to Strava segment" />
-                </div>
-                <div class="form-group">
-                    <label for="challengeStartDate">Challenge start date </label>
-                    <input type="date" class="form-control" id="challengeStartDate" placeholder="Challenge start date" /> 
-                </div>
-                <div class="form-group">
-                    <label for="challengeEndDate">Challenge end date </label>
-                    <input type="date" class="form-control" id="challengeEndDate" placeholder="Challenge end date" /> 
-                </div>
-                <div class="form-group">
-                    <label for="challengePassphrase">Passphrase to enter challenge (auto-generated if left empty) </label>
-                    <input type="text" class="form-control" id="challengePassphrase" placeholder="Passphrase to enter challenge" />
-                </div>
-                <button type="submit" class="btn btn-primary">Add</button>
+                <form id="challengeInputForm" onSubmit={this.saveChallenge}>
+                    <div class="form-group">
+                        <label for="challengeSegment">Link to Strava segment </label>
+                        <input 
+                            type="text" 
+                            class="form-control"
+                            id="challengeSegment"
+                            value={this.state.challengeSegment}
+                            onChange={this.handleChange}
+                            placeholder="Link to Strava segment" />
+                    </div>
+                    <div class="form-group">
+                        <label for="challengeStartDate">Challenge start date </label>
+                        <input 
+                            type="date" 
+                            class="form-control" 
+                            id="challengeStartDate"
+                            value={this.state.challengeStartDate}
+                            onChange={this.handleChange}
+                            placeholder="Challenge start date" /> 
+                    </div>
+                    <div class="form-group">
+                        <label for="challengeEndDate">Challenge end date </label>
+                        <input 
+                            type="date" 
+                            class="form-control" 
+                            id="challengeEndDate" 
+                            value={this.state.challengeEndDate}
+                            onChange={this.handleChange}
+                            placeholder="Challenge end date" /> 
+                    </div>
+                    <div class="form-group">
+                        <label for="challengePassphrase">Passphrase to enter challenge (auto-generated if left empty) </label>
+                        <input 
+                            type="text" 
+                            class="form-control" 
+                            id="challengePassphrase" 
+                            value={this.state.challengePassphrase}
+                            onChange={this.handleChange}
+                            placeholder="Passphrase to enter challenge" />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Add</button>
                 </form>
             </div>
         );
     }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    saveChallenge()
+    {
+        return;
+    }
 }
   
 class StravaChallenge extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          history: [{
-            squares: Array(9).fill(null),
-          }],
-          xIsNext: true,
-        };
-      }
-
     render() {
       return (
         <div className="stravaChallenge">
