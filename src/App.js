8ffdf1e03d9acee61fc865c5a89467b853c448b4
Evan Charlton <evancharlton@gmail.com>
2021-05-31 21:26:44 +0200

    chore: Some small cleanups (#3)
    * chore: Some small cleanup
    
    Apply some small cleanups to the App.js file. Specifically:
    
     1. Check in a `.prettierrc` to define the style
     2. Run App.js through prettier
     3. Remove the unused `logo.svg`
    
    There's probably more to do but it's a start.
    
    * Install prettier & run it on everything in src/
    
    Let's clean it all up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import ChallengeInputForm from './ChallengeInputForm.js';
 
 function App() {
   return (
     <div className="stravaChallenge">
-        <h1>The Strava Challenge</h1>
-        <div className="challengeInputForm">
-            <ChallengeInputForm />
-        </div>
-        <div className="challengeList">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-        </div>
-        <div class="footer">
-            <p>&copy alexdiem.io</p>
-        </div>
+      <h1>The Strava Challenge</h1>
+      <div className="challengeInputForm">
+        <ChallengeInputForm />
+      </div>
+      <div className="challengeList">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+      <div class="footer">
+        <p>&copy; alexdiem.io</p>
+      </div>
     </div>
-  );  
+  );
 }
 
 export default App;
--- src/ChallengeInputForm.js
+++ src/ChallengeInputForm.js
@@ -7,9 +7,9 @@ const ChallengeInputForm = () => {
       challengeSegment: '',
       challengeStartDate: new Date(),
       challengeEndDate: new Date(),
-      challengePassphrase: ''
+      challengePassphrase: '',
     },
-    onSubmit: values => {
+    onSubmit: (values) => {
       alert(JSON.stringify(values));
     },
   });
@@ -23,8 +23,9 @@ const ChallengeInputForm = () => {
             type="text"
             id="challengeSegment"
             onChange={formik.handleChange}
-            value={formik.values.challengeSegment} 
-            placeholder="Link to Strava segment"/>
+            value={formik.values.challengeSegment}
+            placeholder="Link to Strava segment"
+          />
         </div>
         <div class="form-group">
           <label for="challengeStartDate">Challenge start date </label>
@@ -32,7 +33,8 @@ const ChallengeInputForm = () => {
             type="date"
             id="challengeStartDate"
             onChange={formik.handleChange}
-            value={formik.values.challengeStartDate} />
+            value={formik.values.challengeStartDate}
+          />
         </div>
         <div class="form-group">
           <label for="challengeEndDate">Challenge end date </label>
@@ -40,21 +42,25 @@ const ChallengeInputForm = () => {
             type="date"
             id="challengeEndDate"
             onChange={formik.handleChange}
-            value={formik.values.challengeEndDate} />
+            value={formik.values.challengeEndDate}
+          />
         </div>
         <div class="form-group">
-          <label for="challengePassphrase">Passphrase to enter challenge (auto-generated if left empty) </label>
+          <label for="challengePassphrase">
+            Passphrase to enter challenge (auto-generated if left empty){' '}
+          </label>
           <input
             type="text"
             id="challengePassphrase"
             onChange={formik.handleChange}
-            value={formik.values.challengePassphrase} 
-            placeholder="Passphrase to enter challenge"/>
+            value={formik.values.challengePassphrase}
+            placeholder="Passphrase to enter challenge"
+          />
         </div>
         <button type="submit">Add</button>
       </form>
     </div>
   );
-}
+};
 
 export default ChallengeInputForm;
